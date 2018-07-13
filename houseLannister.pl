ce7738085803a993f2d2ed0f23b1a1a9f29ce767
Benjamin Kluck <kluck@sipgate.de>
2018-07-13 14:29:40 +0200

    parentsOf

--- houseLannister.pl
+++ houseLannister.pl
@@ -1,12 +1,12 @@
 motherOf(cersei, joffrey).
 motherOf(cersei, myrcella).
-motherOf(cersei,tommen).
+motherOf(cersei, tommen).
 motherOf(joanna, cersei).
-motherOf(joanna,jaime).
-motherOf(joanna,tyrion).
+motherOf(joanna, jaime).
+motherOf(joanna, tyrion).
 motherOf(dorna, lancel).
 motherOf(dorna, willem).
-motherOf(dorna,martyn).
+motherOf(dorna, martyn).
 
 fatherOf(jaime, joffrey).
 fatherOf(jaime, myrcella).
@@ -14,8 +14,12 @@ fatherOf(jaime, tommen).
 fatherOf(tywin, cersei).
 fatherOf(tywin, jaime).
 fatherOf(tywin, tyrion).
-fatherOf(tytos,tywin).
-fatherOf(tytos,kevan).
+fatherOf(tytos, tywin).
+fatherOf(tytos, kevan).
 fatherOf(kevan, lancel).
 fatherOf(kevan, willem).
 fatherOf(kevan, martyn).
+
+
+parentsOf(X, Y) :- motherOf(X, Y).
+parentsOf(X, Y) :- fatherOf(X, Y).
