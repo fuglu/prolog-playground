6236f156c351abbe64f68bc7c7f520455963a1ce
Benjamin Kluck <kluck@sipgate.de>
2018-07-13 14:13:30 +0200

    Add houseLannister.pl

---
+++ houseLannister.pl
@@ -0,0 +1,21 @@
+motherOf(cersei, joffrey).
+motherOf(cersei, myrcella).
+motherOf(cersei,tommen).
+motherOf(joanna, cersei).
+motherOf(joanna,jaime).
+motherOf(joanna,tyrion).
+motherOf(dorna, lancel).
+motherOf(dorna, willem).
+motherOf(dorna,martyn).
+
+fatherOf(jaime, joffrey).
+fatherOf(jaime, myrcella).
+fatherOf(jaime, tommen).
+fatherOf(tywin, cersei).
+fatherOf(tywin, jaime)).
+fatherOf(tywin, tyrion).
+fatherOf(tytos,tywin).
+fatherOf(tytos,kevan).
+fatherOf(kevan, lancel).
+fatherOf(kevan, willem).
+fatherOf(kevan, martyn).
